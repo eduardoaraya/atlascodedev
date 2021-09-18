6d36ad089620e5d51170e1b63eb6ef89497d4043
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-18 17:01:59 -0300

    [chore] generated react library to host frontend application sections

---
+++ libs/frontend-sections/src/lib/FrontendSections.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FrontendSectionsProps {}
+
+export function FrontendSections(props: FrontendSectionsProps) {
+  return (
+    <div>
+      <h1>Welcome to FrontendSections!</h1>
+    </div>
+  );
+}
+
+export default FrontendSections;
