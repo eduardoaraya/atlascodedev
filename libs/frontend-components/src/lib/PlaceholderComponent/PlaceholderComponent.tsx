ead01f30d27290882fd35212579ac8783554d22c
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-18 16:55:48 -0300

    [config][chore] extend workspace.json file to set default options for the @nrwl/react component generator; generated a frontend-component react library and included a placeholder component in it

---
+++ libs/frontend-components/src/lib/PlaceholderComponent/PlaceholderComponent.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface PlaceholderComponentProps {}
+
+export function PlaceholderComponent(props: PlaceholderComponentProps) {
+  return (
+    <div>
+      <h1>Welcome to PlaceholderComponent!</h1>
+    </div>
+  );
+}
+
+export default PlaceholderComponent;
