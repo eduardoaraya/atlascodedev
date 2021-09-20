dd44b0040eb702ded5560dbf6bd9c95f9833c3ea
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-20 02:25:54 -0300

    [feature] home hero section

--- libs/frontend-sections/src/lib/HomeHero/HomeHero.stories.tsx
+++ libs/frontend-sections/src/lib/HomeHero/HomeHero.stories.tsx
@@ -1,9 +1,23 @@
 import { Story, Meta } from '@storybook/react';
 import { HomeHero, HomeHeroProps } from './HomeHero';
+import { Box } from '@mui/material';
 
 export default {
   component: HomeHero,
   title: 'HomeHero',
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'brazilDesktop1',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Box sx={{ width: '100%', height: '105vh' }}>
+        <Story />
+      </Box>
+    ),
+  ],
 } as Meta;
 
 const Template: Story<HomeHeroProps> = (args) => <HomeHero {...args} />;
