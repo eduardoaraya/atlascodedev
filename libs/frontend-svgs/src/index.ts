37082a84811e8e9d6d301b0a6555d301e1bded9a
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-21 01:51:51 -0300

    [feature] converted about us hero svg image to react component

--- libs/frontend-svgs/src/index.ts
+++ libs/frontend-svgs/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/AboutUsHeroSVG/AboutUsHeroSVG';
 export * from './lib/FramerLogo/FramerLogo';
 export * from './lib/NestLogo/NestLogo';
 export * from './lib/TypescriptLogo/TypescriptLogo';
