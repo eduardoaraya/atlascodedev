d0eaaafc81d98b77a69410b023a3218ee074e3b6
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-21 02:21:42 -0300

    [feature] about us hero section

--- libs/frontend-sections/src/lib/AboutUsHero/AboutUsHero.tsx
+++ libs/frontend-sections/src/lib/AboutUsHero/AboutUsHero.tsx
@@ -1,5 +1,6 @@
 import { useMemoizedMergedObject } from '@atlascode/frontend-hooks';
-import { Box, BoxProps, Theme } from '@mui/material';
+import { AboutUsHeroSVG } from '@atlascode/frontend-svgs';
+import { Box, BoxProps, Container, Theme, Typography } from '@mui/material';
 import { SxProps } from '@mui/system';
 /* eslint-disable-next-line */
 export interface AboutUsHeroProps extends BoxProps {}
@@ -9,7 +10,28 @@ export function AboutUsHero({ sx, ...rest }: AboutUsHeroProps) {
 
   return (
     <Box sx={defaultStylesMemo} {...rest}>
-      <Box className="AtlasCode-AboutUsHero-root"></Box>
+      <Container maxWidth="lg" className="AtlasCode-AboutUsHero-root">
+        <Box className="AtlasCode-AboutUsHero-grid">
+          <Box className="AtlasCode-AboutUsHero-illustration-container">
+            <AboutUsHeroSVG className="AtlasCode-AboutUsHero-illustration" />
+          </Box>
+          <Box className="AtlasCode-AboutUsHero-text-container">
+            <Typography
+              variant="h1"
+              className="
+              AtlasCode-AboutUsHero-text-title
+              "
+            >
+              Criamos boas experiências digitais.
+            </Typography>
+
+            <Typography className="AtlasCode-AboutUsHero-text-aux">
+              Ajudamos nossos clientes a apresentarem seus produtos e serviços
+              de forma eficiente.
+            </Typography>
+          </Box>
+        </Box>
+      </Container>
     </Box>
   );
 }
@@ -18,6 +40,47 @@ export default AboutUsHero;
 
 const defaultStyles = () => {
   return {
-    '.AtlasCode-AboutUsHero-root': {},
+    '.AtlasCode-AboutUsHero-root': {
+      fontSize: '10px',
+
+      '.AtlasCode-AboutUsHero-grid': {
+        display: 'grid',
+        gridTemplateColumns: { xs: '1fr', lg: '50% 50%' },
+        gridTemplateRows: { xs: '50% 50%', lg: '1fr' },
+        width: '100%',
+        height: '100%',
+        columnGap: { lg: 5 },
+        px: { xs: 1.5 },
+      },
+
+      '.AtlasCode-AboutUsHero-illustration-container': {
+        p: { lg: 3 },
+        order: { xs: 1, lg: 0 },
+      },
+
+      '.AtlasCode-AboutUsHero-illustration': {},
+
+      '.AtlasCode-AboutUsHero-text-container': {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        gap: { xs: 2 },
+        order: { xs: 0, lg: 1 },
+      },
+
+      '.AtlasCode-AboutUsHero-text-title': {
+        fontSize: { xs: '2.4em', lg: '3.8em' },
+        fontWeight: 900,
+        color: (theme) => theme.palette.secondary.light,
+        maxWidth: { lg: '20ch' },
+      },
+
+      '.AtlasCode-AboutUsHero-text-aux': {
+        fontSize: { xs: '1.6em', lg: '2.2em' },
+        fontWeight: 700,
+        color: (theme) => theme.palette.secondary.light,
+        maxWidth: { lg: '36ch' },
+      },
+    },
   } as SxProps<Theme>;
 };
